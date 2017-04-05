64c6d11697c347ec5afce57169f3c00acf0300c4
Barry <barry.teoh@perceptionz.net>
2017-04-05 10:30:37 +0800

    Current position tracker and improvements to existing functionality (private functions)

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ var privateapi = require('./lib/private');
 
 module.exports = {
   instrument: (info, callback) => {
-    publicapi.instrumentprice(info, function(instrumentcb) {
+    publicapi.instrumentprice(info, (instrumentcb) => {
       callback(instrumentcb);
-    });
+    })
   },
   openorders: (info, callback) => {
-    privateapi.openorders(info, function(orderscb) {
+    privateapi.openorders(info, (orderscb) => {
       callback(orderscb);
+    })
+  },
+  currentposition: (info, callback) => {
+    privateapi.currentposition(info, (positioncb) => {
+      callback(positioncb);
     })
   }
 };
